8c754d6693ca8111c344195d15d7d1b7a6abd5c1
Kappamalone <uzman.zawahir1@gmail.com>
2020-08-30 21:01:54 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express();
 const port = 3000;
+
+//connect to mongodb, password stored in local js file
+const pass = require('./credentials.js').pass
+let mongoDB = `mongodb+srv://Kappamalone:${pass}@kappamalone-cluster.u10jv.mongodb.net/bloggerify?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB);
+let db = mongoose.connection;
+
+mongoose.insert
+
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
@@ -14,7 +24,6 @@ app.get('/',(req,res) => {
 })
 
 app.get('/getBlogs',(req,res) => {
-
   //Send the blogs to the webpage
   res.send(JSON.stringify({
     name: 'bob'
